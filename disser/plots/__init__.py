1b967d2ae6480334985651e07951d4c6f7914e78
Ryan May <rmay31@gmail.com>
2012-02-28 22:22:56 -0600

    Add an iterator class to simplify making subplot labels.

--- disser/plots/__init__.py
+++ disser/plots/__init__.py
@@ -1,5 +1,5 @@
 from .ppi import PPIPlot
-from .basic import Plot
+from .basic import Plot, LabelGenerator
 from .ctables import get_cmap
 
-__all__ = ['Plot', 'PPIPlot', 'get_cmap']
+__all__ = ['Plot', 'PPIPlot', 'get_cmap', 'LabelGenerator']
--- disser/plots/basic.py
+++ disser/plots/basic.py
@@ -1,2 +1,28 @@
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredText
+
+
 class Plot(object):
     pass
+
+# A way to automatically generate labels for subplots through iteration
+class LabelGenerator(object):
+    def __init__(self, first, loc=2):
+        self.first = first
+        self.loc = loc
+
+    def __iter__(self):
+        item = self.first
+        while True:
+            # loc = 2 is upper left. TODO: Should patch matplotlib to use
+            # same strings as legend
+            at = AnchoredText("%s)" % item, loc=self.loc,
+                prop=dict(size='large'), frameon=True)
+
+            newitem = yield at
+            if newitem:
+                item = newitem
+            else:
+                item = self.next_item(item)
+
+    def next_item(self, current):
+        return chr(ord(current) + 1)
