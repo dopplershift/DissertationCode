9461b7d8dce3adb4f152d95a8a2c6ed1de42f9b3
Ryan May <rmay31@gmail.com>
2012-03-15 23:53:40 -0500

    Refactor multipanel plotting slightly.
    Move out a bunch of the code into helper functions. This way when common
    plots that share colorbars in rows/columns, or not at all are made, the
    tuning of spacing and whatnot can be shared.
    
    It will likely take making quite a few more of these plots using the
    helpers to see what's truly useful and where the commonalities are.

--- disser/plots/defaults.py
+++ disser/plots/defaults.py
@@ -7,8 +7,13 @@ pq.markup.format_units_latex = partial(pq.markup.format_units_latex,
 rings = np.arange(10, 60, 10) * pq.km
 
 from .ctables import get_cmap
+from .basic import LabelGenerator
+from .ppi import PPIPlot
 from disser import datatypes
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+# Default colormaps and norms get set up on import
 datatypes.TypePlotInfo.set_defaults(cmap=get_cmap('Carbone42'))
 datatypes.TypePlotInfo[datatypes.Reflectivity].update(
     norm=plt.Normalize(-20, 70))
@@ -16,3 +21,58 @@ datatypes.TypePlotInfo[datatypes.DopplerVelocity].update(
     norm=plt.Normalize(-30, 30))
 datatypes.TypePlotInfo[datatypes.SpectrumWidth].update(
     norm=plt.Normalize(0, 15))
+
+# Helper for labelling the colorbar
+def setup_cbar(cax, colorartist, units, pad=4):
+    cbar = cax.colorbar(colorartist)
+    if units and units != 'dimensionless':
+        cbar.set_label_text(units)
+    cbar.cbar_axis.labelpad = pad
+
+# Helpers for multi-column plots
+def multipanel_cbar_column(fig, layout, moments, data):
+
+    grid = ImageGrid(fig, (1, 1, 1), nrows_ncols=layout, direction='row',
+        share_all=True, axes_pad=0.45, aspect=True, cbar_mode='edge',
+        cbar_location='bottom', cbar_pad=0.55, cbar_size='10%')
+
+    for m, ax, cax, panel_label in zip(moments, grid, grid.cbar_axes,
+            LabelGenerator('a')):
+        ppi = PPIPlot(data.fields, var=m, ax=ax, rings=rings)
+        panel_label.patch.set_boxstyle("round, pad=0., rounding_size=0.2")
+        ax.add_artist(panel_label)
+        ax.set_title(m)
+        setup_cbar(cax, ppi.mesh, data.fields[m].dimensionality)
+
+    return grid
+
+def multipanel_cbar_row(fig, layout, moments, data):
+    # TODO: The params here are not tuned
+    grid = ImageGrid(fig, (1, 1, 1), nrows_ncols=layout, direction='row',
+        share_all=True, axes_pad=0.45, aspect=True, cbar_mode='edge',
+        cbar_location='right', cbar_pad=0.55, cbar_size='10%')
+
+    for m, ax, cax, panel_label in zip(moments, grid, grid.cbar_axes,
+            LabelGenerator('a')):
+        ppi = PPIPlot(data.fields, var=m, ax=ax, rings=rings)
+        panel_label.patch.set_boxstyle("round, pad=0., rounding_size=0.2")
+        ax.add_artist(panel_label)
+        ax.set_title(m)
+        setup_cbar(cax, ppi.mesh, data.fields[m].dimensionality)
+
+    return grid
+
+def multipanel_cbar_each(fig, layout, moments, data):
+    grid = ImageGrid(fig, (1, 1, 1), nrows_ncols=layout, direction='row',
+        share_all=True, axes_pad=0.70, aspect=True, cbar_mode='each',
+        cbar_location='right', cbar_pad=0.15, cbar_size='10%')
+
+    for m, ax, cax, panel_label in zip(moments, grid, grid.cbar_axes,
+            LabelGenerator('a')):
+        ppi = PPIPlot(data.fields, var=m, ax=ax, rings=rings)
+        panel_label.patch.set_boxstyle("round, pad=0., rounding_size=0.2")
+        ax.add_artist(panel_label)
+        ax.set_title(m)
+        setup_cbar(cax, ppi.mesh, data.fields[m].dimensionality)
+
+    return grid
--- scripts/6-panel-single.py
+++ scripts/6-panel-single.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import ImageGrid
 from disser.io import NetCDFRadarData
-from disser.plots import PPIPlot, LabelGenerator, get_cmap
+from disser.plots import get_cmap
 from disser import datatypes
 import disser.plots.defaults as defaults
 
@@ -15,19 +14,11 @@ moments = [data.fields.grab(moment, pol='H', source=src)
            datatypes.DopplerVelocity, datatypes.SpectrumWidth)]
 
 fig = plt.figure()
-grid = ImageGrid(fig, (1, 1, 1), nrows_ncols = (2, 3), direction='row',
-    share_all=True, axes_pad=0.6, aspect=True, cbar_mode='each',
-    cbar_location='right', cbar_pad=0.15, cbar_size='10%')
 
-for m, ax, cax, panel_label in zip(moments, grid, grid.cbar_axes, LabelGenerator('a')):
-    ppi = PPIPlot(data.fields, var=m, ax=ax, rings=defaults.rings)
-    panel_label.patch.set_boxstyle("round, pad=0., rounding_size=0.2")
-    ax.add_artist(panel_label)
-    ax.set_title(m)
-    cbar = cax.colorbar(ppi.mesh)
-    cbar.set_label_text(data.fields[m].dimensionality)
-    cbar.cbar_axis.labelpad = -4
+grid = defaults.multipanel_cbar_column(fig, (2,3), moments, data)
+ax = grid[0]
 
+ax.xaxis.set_major_locator(plt.MultipleLocator(10))
 ax.set_ylim(0, 50)
 ax.set_xlim(-20, 20)
 
