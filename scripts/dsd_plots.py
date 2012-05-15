d012d13b65ba2c3223dc6b6ad46e5bcf43da9014
Ryan May <rmay31@gmail.com>
2012-05-15 01:26:40 -0500

    Working towards making things use Quantities for units.

--- scripts/dsd_plots.py
+++ scripts/dsd_plots.py
@@ -5,24 +5,27 @@ import dsd
 import scipy.constants as consts
 import quantities as pq
 
-d = np.linspace(0.01, 0.8, 100) * pq.centimeter
+d = np.linspace(0.01, 8, 100) * pq.mm
 #d0_lut = (np.linspace(0.01, .5, 40).reshape(1, -1, 1).astype(np.float32)
 #        * consts.centi)
 #nr_lut = np.logspace(-2, 6, 110).reshape(1, 1,-1).astype(np.float32)
 
-trials = [(0.2, 1e4, -0.8, 'k'), (0.4, 1e4, -0.8, 'r'), (0.1, 1e4, -0.8, 'b'),
-          (0.2, 1e5, -0.8, 'r--'), (0.2, 1e3, -0.8, 'b--'),
-          (0.2, 1e4, 0.1, 'r:'), (0.2, 1e4, 0.8, 'b:')]
-for d0,Nr,nu,plot in trials:
+trials = [(5 / pq.mm, 1e4 / pq.m**3, 'k'), (10 / pq.mm, 1e4 / pq.m**3, 'r'),
+          (15 / pq.mm, 1e4 / pq.m**3, 'b'), (20 / pq.mm, 1e4 / pq.m**3, 'g'),
+          (5 / pq.mm, 1e5 / pq.m**3, 'k--'), (10 / pq.mm, 1e5 / pq.m**3, 'r--'),
+          (15 / pq.mm, 1e5 / pq.m**3, 'b--'), (20 / pq.mm, 1e5 / pq.m**3, 'g--')]
+for lam,Nr,plot in trials:
     #dist = dsd.gamma(d, d0_lut, nr_lut, nu=-0.8)
-    dist = dsd.gamma(d, d0 * pq.centimeter, Nr / pq.meter**3, nu)
+    nu = dsd.constrained_gamma_shape(lam)
+    dist = dsd.modified_gamma(d, lam, Nr, nu)
     plt.semilogy(d, dist, plot,
-            label=r'$d_0:%.1f\, N_r:%.0e\, \nu:%.1f$' % (d0,Nr,nu))
+            label=r'$\Lambda:%.1f\, N_r:%.0e\, \nu:%.1f$' % (lam,Nr,nu))
+    print dsd.lwc(d, dist).simplified, dsd.constrained_gamma_shape(lam)
 
 plt.legend(loc='lower right')
 plt.title('DSD Comparison')
 plt.xlabel('Diameter (%s)' % d.dimensionality.latex)
 plt.ylabel('Number (%s)' % dist.dimensionality.latex)
-plt.ylim(1e-2, None)
+plt.ylim(1e1, None)
 plt.grid()
 plt.show()
--- scripts/scattering_calcs.py
+++ scripts/scattering_calcs.py
@@ -3,21 +3,23 @@ import numpy as np
 import scattering
 import dsd
 import scipy.constants as consts
-from disser.units import to_dB
+from disser.units import to_dB, to_dBz
+import quantities as pq
 
 d = (np.linspace(0.01, 0.8, 100).reshape(-1, 1, 1).astype(np.float32) 
-        * consts.centi)
-d0_lut = (np.linspace(0.01, .5, 40).reshape(1, -1, 1).astype(np.float32)
-        * consts.centi)
-nr_lut = np.logspace(-2, 6, 110).reshape(1, 1,-1).astype(np.float32)
-dist = dsd.gamma(d, d0_lut, nr_lut, nu=-0.8)
+        * pq.cm)
+lam = (np.linspace(0.01, 15, 40).reshape(1, -1, 1).astype(np.float32)
+        / pq.mm)
+nr_lut = (np.logspace(-2, 6, 110).reshape(1, 1,-1).astype(np.float32)
+        / (pq.m**3 * pq.cm))
+dist = dsd.modified_gamma(d, lam, nr_lut, dsd.constrained_gamma_shape(lam))
+print dist.dimensionality
 
-wavelength = 0.053
+wavelength = 0.1 * pq.m
 temps = np.arange(0, 35.0, 10.0)
 
 db_factor = 10.0 * np.log10(np.e)
-ref_adjust = 180
-d_plot = d.squeeze() / consts.centi
+d_plot = d.squeeze()
 
 z = np.empty((temps.size,) + dist.shape[1:], dtype=np.float32)
 zdr = np.empty_like(z)
@@ -49,86 +51,87 @@ for ind, t in enumerate(temps):
     diff_phase = (scatt.S_frwd[0, 0] - scatt.S_frwd[1, 1]).real.squeeze()
     ax[1,2].plot(d_plot, diff_phase, label=plot_label)
 
-    z[ind] = to_dB(scatt.get_reflectivity_factor(dist, polar='h'))
-    zdr[ind] = z[ind] - scatt.get_reflectivity_factor(dist, polar='v')
-    atten[ind] = scatt.get_attenuation(dist, polar='h') * db_factor
-    diff_atten[ind] = (atten[ind] -
-            scatt.get_attenuation(dist, polar='v') * db_factor)
-    kdp[ind] = (scatt.get_propagation_wavenumber(dist, polar='h') -
+    z = to_dBz(scatt.get_reflectivity_factor(dist, polar='h'))
+    zdr = z - to_dBz(scatt.get_reflectivity_factor(dist, polar='v'))
+    atten = (scatt.get_attenuation(dist, polar='h').rescale(1/pq.km) *
+            db_factor)
+    diff_atten = (atten -
+            scatt.get_attenuation(dist, polar='v').rescale(1/pq.km) * db_factor)
+    kdp = (scatt.get_propagation_wavenumber(dist, polar='h') -
             scatt.get_propagation_wavenumber(dist, polar='v'))
-    delta[ind] = scatt.get_backscatter_differential_phase(dist)
+    delta = scatt.get_backscatter_differential_phase(dist)
     temp_color = plt.get_cmap('RdBu')(plt.Normalize(0,30)(t))
     scatter_kwargs = dict(c=temp_color, edgecolor='none', alpha=0.4)
-    ax2[0,0].scatter(qr, z[ind] + ref_adjust, **scatter_kwargs)
-    ax2[0,1].scatter(qr, atten[ind], **scatter_kwargs)
-    ax2[0,2].scatter(qr, delta[ind], **scatter_kwargs)
-    ax2[1,0].scatter(qr, zdr[ind], **scatter_kwargs)
-    ax2[1,1].scatter(qr, diff_atten[ind], **scatter_kwargs)
-    ax2[1,2].scatter(qr, kdp[ind], **scatter_kwargs)
+    ax2[0,0].scatter(qr, z, **scatter_kwargs)
+    ax2[0,1].scatter(qr, atten, **scatter_kwargs)
+    ax2[0,2].scatter(qr, delta, **scatter_kwargs)
+    ax2[1,0].scatter(qr, zdr, **scatter_kwargs)
+    ax2[1,1].scatter(qr, diff_atten, **scatter_kwargs)
+    ax2[1,2].scatter(qr, kdp, **scatter_kwargs)
 #    ref = to_dB(scatt.get_reflectivity_factor(dist)) + ref_adjust
 #    atten = scatt.get_attenuation(dist) * consts.kilo * db_factor
-    ax3[0].scatter(kdp[ind], atten[ind], **scatter_kwargs)
-    ax3[1].scatter(kdp[ind], diff_atten[ind], **scatter_kwargs)
+    ax3[0].scatter(kdp, atten, **scatter_kwargs)
+    ax3[1].scatter(kdp, diff_atten, **scatter_kwargs)
 
 
-ax[0,0].set_xlabel('Diameter')
+ax[0,0].set_xlabel('Diameter (%s)' % d_plot.dimensionality)
 ax[0,0].set_ylabel('Backscatter Cross-Section')
 ax[0,0].legend(loc='upper left')
 ax[0,0].grid()
-ax[0,0].set_xlim(0, d_plot.max())
+ax[0,0].set_xlim(0, d_plot.max().magnitude)
 
-ax[0,1].set_xlabel('Diameter')
+ax[0,1].set_xlabel('Diameter (%s)' % d_plot.dimensionality)
 ax[0,1].set_ylabel('Horizontal Extinction Cross-Section')
 ax[0,1].grid()
 
-ax[0,2].set_xlabel('Diameter')
+ax[0,2].set_xlabel('Diameter (%s)' % d_plot.dimensionality)
 ax[0,2].set_ylabel('Differential Backscatter Phase')
 ax[0,2].grid()
 
-ax[1,0].set_xlabel('Diameter')
+ax[1,0].set_xlabel('Diameter (%s)' % d_plot.dimensionality)
 ax[1,0].set_ylabel('Differential Backscatter Cross-Section')
 ax[1,0].grid()
 
-ax[1,1].set_xlabel('Diameter')
+ax[1,1].set_xlabel('Diameter (%s)' % d_plot.dimensionality)
 ax[1,1].set_ylabel('Differential Extinction Cross-Section')
 ax[1,1].grid()
 
-ax[1,2].set_xlabel('Diameter')
+ax[1,2].set_xlabel('Diameter (%s)' % d_plot.dimensionality)
 ax[1,2].set_ylabel('Differential Wavenumber')
 ax[1,2].grid()
 
-ax2[0,0].set_xlabel('Rain Water')
+ax2[0,0].set_xlabel('Rain Water (%s)' % qr.dimensionality)
 ax2[0,0].set_ylabel('Reflectivity Factor')
 ax2[0,0].grid()
 ax2[0,0].set_xlim(0, 35)
 
-ax2[0,1].set_xlabel('Rain Water')
+ax2[0,1].set_xlabel('Rain Water (%s)' % qr.dimensionality)
 ax2[0,1].set_ylabel('Attenuation')
 ax2[0,1].grid()
 
-ax2[0,2].set_xlabel('Rain Water')
+ax2[0,2].set_xlabel('Rain Water (%s)' % qr.dimensionality)
 ax2[0,2].set_ylabel('Differential Backscatter Phase')
 ax2[0,2].grid()
 
-ax2[1,0].set_xlabel('Rain Water')
+ax2[1,0].set_xlabel('Rain Water (%s)' % qr.dimensionality)
 ax2[1,0].set_ylabel('Differential Reflectivity')
 #ax2[1,0].set_ylim(-6,6)
 ax2[1,0].grid()
 
-ax2[1,1].set_xlabel('Rain Water')
+ax2[1,1].set_xlabel('Rain Water (%s)' % qr.dimensionality)
 ax2[1,1].set_ylabel('Differential Attenuation')
 ax2[1,1].grid()
 
-ax2[1,2].set_xlabel('Rain Water')
+ax2[1,2].set_xlabel('Rain Water (%s)' % qr.dimensionality)
 ax2[1,2].set_ylabel('KDP')
 ax2[1,2].grid()
 
-ax3[0].set_xlabel('KDP')
+ax3[0].set_xlabel('KDP (%s)' % kdp.dimensionality)
 ax3[0].set_ylabel('Attenuation')
 ax3[0].grid()
 ax3[0].set_xlim(0,3)
 
-ax3[1].set_xlabel('KDP')
+ax3[1].set_xlabel('KDP (%s)' % kdp.dimensionality)
 ax3[1].set_ylabel('Differential Attenuation')
 ax3[1].grid()
 
