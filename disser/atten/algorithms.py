f615a35203e117b3a1c67b13f3bdb5086097cc7a
Ryan May <rmay31@gmail.com>
2012-11-26 22:07:46 -0600

    Wrap long lines.

--- disser/atten/algorithms.py
+++ disser/atten/algorithms.py
@@ -5,23 +5,27 @@ import quantities as pq
 from ..units import dB
 from fit_coeffs import za_coeffs, ka_coeffs, sc_coeffs
 
-# This is the main calculation, as outlined in equation (24) in the Testud et al. (2000) paper
+# This is the main calculation, as outlined in equation (24) in the Testud
+# et al. (2000) paper
 def zphi(z, dr, delta_phi, b=0.7644, gamma=3.2e-1):
     phi_factor = np.expm1(np.log(10) * b * gamma * delta_phi / 10.)
-    return phi_factor * 10**((b / 10.) * z) / (I0(z, dr, b) + phi_factor * I(z, dr, b))
+    return phi_factor * 10**((b / 10.) * z) /
+        (I0(z, dr, b) + phi_factor * I(z, dr, b))
 
-# This is the implementation of the I function. We have two versions, since one needs to be
-# implemented as a cumulative function in order to maintain a dependence on r.
+# This is the implementation of the I function. We have two versions, since
+# one needs to be implemented as a cumulative function in order to maintain
+# a dependence on r.
 
-# This is what the 0.46 really is. Using just 0.46 introduces a 1% error needlessly.
+# This is what the 0.46 really is. Using just 0.46 introduces a 1% error
 db_conv = (2. / 10.) * np.log(10)
 
-# Z needs to be in dB here. This is why the formula differs from the paper, which is linear.
-# Keeping Z in dB let's us only need a single pow (**) call.
+# Z needs to be in dB here. This is why the formula differs from the paper,
+# which is linear. Keeping Z in dB let's us only need a single pow (**) call.
 def I(z, dr, b):
-    # Cumulatively integrate away from the last value. Return these in the reverse order since
-    # it's supposed to be in order of increasing r.
-    return db_conv * b * si.cumtrapz(10 ** ((b / 10.)* z[::-1]), dx=dr, initial=0)[::-1]
+    # Cumulatively integrate away from the last value. Return these in the
+    # reverse order since it's supposed to be in order of increasing r.
+    return db_conv * b * si.cumtrapz(10 ** ((b / 10.)* z[::-1]), dx=dr,
+        initial=0)[::-1]
 
 # And here too
 def I0(z, dr, b):
